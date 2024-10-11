edca27c8f6574ce07c166d67d485e931ae728ee7
ruddy-tools <toolsforhumanity@proton.me>
2024-10-11 16:11:19 +0000

    Adapt to codespace

--- run.py
+++ run.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
-run_list = range(0,419)
-run_depth = 2
-
-from subprocess import call
-from concurrent.futures import ThreadPoolExecutor as Pool
 import os
 import sys
-from random import *
+from random import randint
+import time
+import subprocess
+from concurrent.futures import ThreadPoolExecutor as Pool
 
-version = str(randint(0,10**9))
-if len(sys.argv) == 2: version = sys.argv[1]
+run_list = range(0, 419)
+run_depth = 2
+
+version = str(randint(0, 10**9))
+if len(sys.argv) == 2:
+    version = sys.argv[1]
 print("Updating to version", version)
 
 parallel = 6
@@ -19,38 +21,47 @@ os.system('mkdir -p store/version/')
 os.system('mkdir -p store/tmp/')
 
 def outdated(i):
-    fn = 'store/version/%d.txt'%i
-    os.system('touch '+fn)
-    f = open(fn, 'r')
-    t = f.read()
-    f.close()
+    fn = f'store/version/{i}.txt'
+    os.system(f'touch {fn}')
+    with open(fn, 'r') as f:
+        t = f.read()
     return t != version
+
 def update(i):
-    fn = 'store/version/%d.txt'%i
-    os.system('touch '+fn)
-    f = open(fn, 'w')
-    f.write(version)
-    f.close()
+    fn = f'store/version/{i}.txt'
+    with open(fn, 'w') as f:
+        f.write(version)
 
 run_list = [i for i in run_list if outdated(i)]
 
-global done
 done = 0
-
 n = len(run_list)
+
+def timed_run(command, stdout_file, stderr_file):
+    start_time = time.time()
+    with open(stdout_file, 'w') as out, open(stderr_file, 'w') as err:
+        result = subprocess.run(command, stdout=out, stderr=err)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    return result.returncode, execution_time
+
 def run(i):
-    if call(['/usr/bin/time', './run', str(i), str(run_depth)], stdout=open('store/tmp/%d_out.txt'%i,'w'), stderr=open('store/tmp/%d_err.txt'%i,'w')):
-        print(i, "Crashed")
-        return i
-    os.system('cp store/tmp/%d_out.txt store/%d_out.txt'%(i,i))
-    update(i)
     global done
+    returncode, exec_time = timed_run(['./run', str(i), str(run_depth)], 
+                                      f'store/tmp/{i}_out.txt', 
+                                      f'store/tmp/{i}_err.txt')
+    if returncode:
+        print(f"{i} Crashed")
+        return i
+    os.system(f'cp store/tmp/{i}_out.txt store/{i}_out.txt')
+    update(i)
     done += 1
-    print("%d / %d     \r"%(done, n), end = "")
+    print(f"{done} / {n}     \r", end="")
     sys.stdout.flush()
+    print(f"Task {i} completed in {exec_time:.2f} seconds")
     return i
 
-call(['make','-j'])
+subprocess.run(['make', '-j'])
 scores = []
 with Pool(max_workers=parallel) as executor:
     for i in executor.map(run, run_list):
